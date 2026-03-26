56968e1e165022b858ef1382a5c5acc4da22c858
owenhuang99 <hhuang2@tepper.cmu.edu>
2026-03-26 10:42:27 -0400

    change app.py inline comment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 from core import process_new_link, mark_card_status, load_all_unlearned_cards, generate_cards_for_url, reset_learned, canonicalize_url,load_unlearned_cards,load_csv
 
-
+## TESTING comment for mintlify testing
 st.set_page_config(page_title="Link Learning App", layout="wide")
 
 # fix
